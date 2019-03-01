d83396d89095da8df2a3ae9fa632b11c56590a42
marco970 <m.kuciak@gmail.com>
2019-03-01 13:15:45 +0100

    klasa read

--- AsapDB_3/src/pl/asap/transactions/ReadTrans.java
+++ AsapDB_3/src/pl/asap/transactions/ReadTrans.java
@@ -9,6 +9,7 @@ import pl.asap.entity.Lista;
 public class ReadTrans extends TransBlank{
 	
 	public Object[][] dane;
+	public int rowNr;
 
 	public ReadTrans(Object bean) {
 		super(bean);
@@ -16,8 +17,7 @@ public class ReadTrans extends TransBlank{
 		session.beginTransaction();
 		
 		String select = "from Lista";
-		
-		
+				
 		Query query = session.createQuery(select);
 		List<Lista> result = query.getResultList();
 		
@@ -26,8 +26,8 @@ public class ReadTrans extends TransBlank{
 		
 		int i = result.get(0).getLength();	//kolumny
 		int j = result.size();	//wiersze
-		System.out.println("kolumny i: "+i);
-		System.out.println("wiersze j: "+j);
+		//System.out.println("kolumny i: "+i);
+		//System.out.println("wiersze j: "+j);
 		
 		dane = new Object[i-1][j-1];
 		int k;
@@ -38,23 +38,38 @@ public class ReadTrans extends TransBlank{
 			for (Object el: obj)	{
 				if (!(k==0 || l==0))	{
 				dane[k-1][l-1] = el;
-				System.out.print(el+"-("+k+"|"+l+") ");
+				//System.out.print(el+"-("+k+"|"+l+") ");
 				}
 				k++;
 			}
 			l++;
-			System.out.println();
+			//System.out.println();
 		}
-		System.out.println("kolumny m: "+dane.length);
-		System.out.println("wiersze n: "+dane[0].length);
+		//System.out.println("kolumny m: "+dane.length);
+		//System.out.println("wiersze n: "+dane[0].length);
+		/*
 		for(int n = 0; n<=j-2; n++)	{
 			for (int m =0; m<=i-2; m++)	{
 				System.out.print(dane[m][n]+"-("+m+"|"+n+") ");
 			}
 			System.out.println();
-			
-		}
-		
+		}	*/
 	}
+	public Object[][] getMatrix()	{
+		return dane;
+	}
+	public int rowNr()	{
+		return dane[0].length;
+	}
+	
+	/*public int getID(int rowNr)	{
+		int id = 0;
+		return id;
+	}
+	public int getID(String r)	{
+		int id = 0;
+		return id;
+	} */
+	
 
 }
